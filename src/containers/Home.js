15201b3025ef5dd641ac7adfc57ffab5be10f439
kol <kol@me.com>
2019-06-14 12:47:13 +0100

    Initial Commit

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Home.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>React App</h1>
+          <p>A simple react app</p>
+        </div>
+      </div>
+    );
+  }
+}
